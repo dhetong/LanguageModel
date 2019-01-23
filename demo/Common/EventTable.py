74a3b944d1a4d343df153088a0bb3440cf806ef5
HetongD <dhetong0714@gmail.com>
2019-01-23 14:10:27 -0500

    Event Table

--- demo/Common/EventTable.py
+++ demo/Common/EventTable.py
@@ -3,4 +3,13 @@ import demo.Common.Event
 class EventTable:
     'Read and Write the Event Table'
     #The path of the file storing log event table
-    filePath = 'demo/RelevantFiles/EventTable.txt';
+    def __init__(self):
+        self.filePath = 'demo/RelevantFiles/EventTable.txt';
+
+    def _TransformFormat(self):
+    #TODO: transfrom the log events' format of other log parsing method into our log event format
+        pass
+
+    def ReadEventByID(self, index):
+    #TODO: read the log event from the event table according to the ID of log event
+        pass
